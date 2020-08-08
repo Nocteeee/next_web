27813744c1e9600ab876395b84759b3bd9a8516b
liunian <965205737@qq.com>
2020-08-08 23:20:30 +0800

    fix: dix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withCss = require('@zeit/next-css')
-const withPurgeCss = require('next-purgecss')
+// const withCss = require('@zeit/next-css')
+// const withPurgeCss = require('next-purgecss')
 
-module.exports = withCss(withPurgeCss({
-    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer ,
-    purgeCssPaths: [
-        './public/**/*.css',
-        './pages/*.js',
-        './component/*.js',
-    ]
-}))
+// module.exports = withCss(withPurgeCss({
+//     purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer ,
+//     purgeCssPaths: [
+//         './public/**/*.css',
+//         './pages/*.js',
+//         './component/*.js',
+//     ]
+// }))
